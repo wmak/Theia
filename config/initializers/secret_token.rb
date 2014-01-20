3994a36f29b0ea8a11ba759195f66df673674316
William Mak <william.mak@hotmail.ca>
2014-01-20 12:30:31 -0500

    Adding everything else

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Theia::Application.config.secret_key_base = '05936cdd3b9a9ba964021846fc366fbf6e0eb4b194fa6ff4886def98f7314489b65e06a65f07404ac070d0ad048227cde506a86bb50a481a2707d3c891ca8c1f'
